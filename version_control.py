a36c0f211f5a7cd6de81058fe4e25fba29f66a99
abhinavyarragunta <yarragunta.a@ufl.edu>
2023-10-25 10:19:34 -0400

    encoding method

--- version_control.py
+++ version_control.py
@@ -1,11 +1,15 @@
-def main():
-    password = list(input('Enter password: '))
+def encode(password):
     for i in range(len(password)):
         password[i] = int(password[i])
         password[i] += 3
         password[i] = str(password[i])
     password = ''.join(password)
-    print(password)
+    return password
+
+
+def main():
+    password = list(input('Enter password: '))
+    print(encode(password))
 
 
 if __name__ == '__main__':
