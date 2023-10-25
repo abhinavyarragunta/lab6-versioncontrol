1d5fd6c7cae319152576ab9dab0efcc9e02868bd
abhinavyarragunta <yarragunta.a@ufl.edu>
2023-10-25 11:13:34 -0400

    encoding and main method

--- version_control.py
+++ version_control.py
@@ -1,10 +1,13 @@
 def encode(password):
+    password = list(password)
     for i in range(len(password)):
         password[i] = int(password[i])
         password[i] += 3
+        password[i] %= 10
         password[i] = str(password[i])
     password = ''.join(password)
     return password
+
 
 def decode(password):
     a = ''
@@ -15,9 +18,24 @@ def decode(password):
         a = a + str(b)
     return a
 
+
 def main():
-    password = list(input('Enter password: '))
-    print(encode(password))
+    global password, encoded_password
+    quit_program = False
+    while not quit_program:
+        menu = 'Menu'
+        print(menu)
+        print('-' * len(menu))
+        print('1. Encode\n2. Decode\n3. Quit')
+        menu_option = input('Please enter an option: ')
+        if menu_option == '1':
+            password = input('Please enter your password to encode: ')
+            encoded_password = encode(password)
+            print('Your password has been encoded and stored!')
+        elif menu_option == '2':
+            print(f'The encoded password is {encoded_password}, and the original password is {password}.')
+        elif menu_option == '3':
+            quit_program = True
 
 
 if __name__ == '__main__':
